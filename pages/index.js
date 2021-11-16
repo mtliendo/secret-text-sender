6e5f91a79ffaa032a289d9d6e7323fbcc9f787f4
Michael Liendo <mtliendo@amazon.com>
2021-11-15 23:29:34 -0600

    complete initial scaffold

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,62 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {
+	Box,
+	Button,
+	Input,
+	Table,
+	Thead,
+	Tbody,
+	Tr,
+	Th,
+	Td,
+	InputGroup,
+	InputLeftElement,
+} from '@chakra-ui/react'
+import { useState } from 'react'
 
 export default function Home() {
-	return <div className={styles.container}></div>
+	const [phoneData, setPhoneData] = useState([
+		{ number: '+15555555555', alias: 'Dad' },
+		{ number: '+14444444444', alias: 'Sister' },
+	])
+
+	return (
+		<Box>
+			<Box>
+				<InputGroup>
+					<InputLeftElement
+						pointerEvents="none"
+						color="gray.600"
+						fontSize="1.2em"
+						// eslint-disable-next-line react/no-children-prop
+						children="$"
+					/>
+					<Input placeholder="Enter budget amount" />
+				</InputGroup>
+			</Box>
+
+			<Box>
+				<Table variant="simple">
+					<Thead>
+						<Tr>
+							<Th>Verified Phone Number</Th>
+							<Th>Alias</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{phoneData.map((phoneItem) => (
+							<Tr key={phoneItem.number}>
+								<Td>{phoneItem.number}</Td>
+								<Td>{phoneItem.alias}</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</Box>
+			<Box>
+				<Button colorScheme="teal" variant="solid">
+					Randomize and Send!
+				</Button>
+			</Box>
+		</Box>
+	)
 }
